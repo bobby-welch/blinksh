aacb923eec50b725a64c2f49a2b8a95d9fbb75aa
Bobby Welch <bobby@ubuntu-18.04>
2019-03-17 13:24:55 -0400

    Add Onedark theme

---
+++ onedark.js
@@ -0,0 +1,47 @@
+// OneDark
+//
+
+base00 = '#282c34';
+base01 = '#e06c75';
+base02 = '#98c379';
+base03 = '#e5c07b';
+base04 = '#61afef';
+base05 = '#c678dd';
+base06 = '#56b6c2';
+base07 = '#abb2bf';
+base08 = '#3e4452';
+base09 = '#be5046';
+base0A = '#98c379';
+base0B = '#d19a66';
+base0C = '#61afef';
+base0D = '#c678dd';
+base0E = '#56b6c2';
+base0F = '#5c6370';
+
+t.prefs_.set('color-palette-overrides', 
+                        [base00,
+                        base08,
+                        base0B,
+                        base0A,
+                        base0D,
+                        base0E,
+                        base0C,
+                        base05,
+                        base03,
+                        base08,
+                        base0B,
+                        base0A,
+                        base0D,
+                        base0E,
+                        base0C,
+                        base07,
+                        base09,
+                        base0F,
+                        base01,
+                        base02,
+                        base04,
+                        base06]);
+
+t.prefs_.set('cursor-color', "rgba(171, 178, 191, 0.5)");
+t.prefs_.set('foreground-color', base05);
+t.prefs_.set('background-color', base00);
