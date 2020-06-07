acd1a409adb79f1a10ee0682dd9b143c0d44ef59
Bobby Welch <bobby@welch.xyz>
2020-06-07 15:59:59 -0400

    Add Pale Night theme

---
+++ palenight.js
@@ -0,0 +1,5 @@
+// https://github.com/base16-templates/base16-xresources/blob/master/xresources/base16-material-palenight-256.Xresources
+t.prefs_.set('color-palette-overrides', ["#292d3e", "#f07178", "#c3e88d", "#ffcb6b", "#82aaff", "#c792ea", "#89ddff", "#959dcb", "#676e95", "#f07178", "#c3e88d", "#ffcb6b", "#82aaff", "#c792ea", "#89ddff", "#ffffff"];
+t.prefs_.set('foreground-color', "#959dcb");
+t.prefs_.set('background-color', "#292d3e");
+t.prefs_.set('cursor-color', 'rgba(149, 157, 203, 0.5)');
