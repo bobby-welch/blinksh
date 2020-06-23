843832dc06764282c13779cec71905c330715584
Bobby Welch <bobby@welch.xyz>
2020-06-22 21:13:09 -0400

    Update dracula theme

--- dracula.js
+++ dracula.js
@@ -1,49 +1,49 @@
 // https://spec.draculatheme.com/#sec-Example-spec-compliant-theme-implementation
 { 
     // dracula theme
-    const black = '#21222C';
-    const red = '#FF5555';
-    const green = '#50FA7B';
-    const yellow '#F1FA8C';
-    const blue = '#BD93F9';
-    const magenta = '#FF79C6';
-    const cyan = '#8BE9FD';
-    const white = '#F8F8F2';
-    const brightBlack = '#6272A4';
-    const brightRed = '#FF6E6E';
-    const brightGreen = '#69FF94';
-    const brightYellow = '#FFFFA5';
-    const brightBlue = '#D6ACFF';
-    const brightMagenta = '#FF92DF';
-    const brightCyan = '#A4FFFF';
-    const brightWhite = '#FFFFFF';
-    const background = '282A36;'
-    const foreground = 'F8F8F2;'
+    let black = '#21222C';
+    let red = '#FF5555';
+    let green = '#50FA7B';
+    let yellow '#F1FA8C';
+    let blue = '#BD93F9';
+    let magenta = '#FF79C6';
+    let cyan = '#8BE9FD';
+    let white = '#F8F8F2';
+    let brightBlack = '#6272A4';
+    let brightRed = '#FF6E6E';
+    let brightGreen = '#69FF94';
+    let brightYellow = '#FFFFA5';
+    let brightBlue = '#D6ACFF';
+    let brightMagenta = '#FF92DF';
+    let brightCyan = '#A4FFFF';
+    let brightWhite = '#FFFFFF';
+    let background = '282A36';
+    let foreground = 'F8F8F2';
 
     // black dark/light
-    const color0 = black;
-    const color8 = brightBlack;
+    let color0 = black;
+    let color8 = brightBlack;
     // red dark/light
-    const color1 = red;
-    const color9 = brightRed;
+    let color1 = red;
+    let color9 = brightRed;
     // green dark/light
-    const color2 = green;
-    const color10 = brightGreen;
+    let color2 = green;
+    let color10 = brightGreen;
     // yellow dark/light
-    const color3 = yellow;
-    const color11 = brightYellow;
+    let color3 = yellow;
+    let color11 = brightYellow;
     // blue dark/light
-    const color4 = blue;
-    const color12 = brightBlue;
+    let color4 = blue;
+    let color12 = brightBlue;
     // magenta dark/light
-    const color5 = magenta;
-    const color13 = brightMagenta;
+    let color5 = magenta;
+    let color13 = brightMagenta;
     // cyan dark/light
-    const color6 = cyan;
-    const color14 = brightCyan;
+    let color6 = cyan;
+    let color14 = brightCyan;
     // white dark/light
-    const color7 = white;
-    const color15 = brightWhite;
+    let color7 = white;
+    let color15 = brightWhite;
 
     // set preferences
     t.prefs_.set('color-palette-overrides', [color0, color1, color2, color3, color4, color5, color6, color7, color8, color9, color10, color11, color12, color13, color14, color15]);
